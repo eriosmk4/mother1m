d778371b1605efefc17986194b98741a101441e0
eriosmk4 <eriosmk4.01@protonmail.ch>
2019-10-20 22:10:38 +1300

    extract.py: Implement getByteAt()

--- extract.py
+++ extract.py
@@ -8,4 +8,17 @@ rom_bits = bitarray()
 with open(rom_path, 'rb') as f:
     rom_bits.fromfile(f)
 
-print(len(rom_bits))
+rom_map = {
+    'map_data': {
+        'starts': 0x2010,
+        'ends':   0x2000F
+    }
+}
+
+def getByteAt(loc, bits):
+    loc_decimal = int(loc)
+    index = loc_decimal * 8
+
+    return bits[index:index + 8]
+
+print(getByteAt(rom_map['map_data']['starts'], rom_bits))
